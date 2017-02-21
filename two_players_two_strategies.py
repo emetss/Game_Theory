beefa34c35333769bd0fd2c60ad572d4fa17de72
rappatoni <maxgrapp@googlemail.com>
2017-02-21 23:03:00 +0100

    first attempt

---
+++ two_players_two_strategies.py
@@ -0,0 +1,23 @@
+import numpy as np
+#Finds all pure and mixed Nash-Equilibria of any two-players-two-strategies-game
+
+
+class game(object):
+    '''A two-players-two-strategies-game'''
+
+    a_strategies=("a","b")
+    b_strategies=("a","b")
+    a_payoff=
+    b_payoff=
+
+    def __init__(self):
+        '''Constructor
+        '''
+
+        pass
+
+    def import_game(self, data_path):
+          '''Reads a text file in the correct format and builds the game accordingly
+
+        :param data_path: Path to the text file.'''
+
