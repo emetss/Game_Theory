a4655bf5e3dcddb5eaa7e7308a1337fe675a5235
rappatoni <maxgrapp@googlemail.com>
2017-02-27 13:32:13 +0100

    first version of fictitious play

--- fictitous_play.py
+++ fictitous_play.py
@@ -1,7 +1,7 @@
 import numpy as np
 from random import choice
 
-dummy_game=np.array([1,-1,2,-2])
+dummy_game=np.array([2,-2,2,-2])
 
 
 class ZeroSumGame(object):
@@ -26,7 +26,7 @@ class ZeroSumGame(object):
         return
         pass
 
-    def fictitiousPlay (self, epsilon):
+    def fictitiousPlay (self, epsilon_1, epsilon_2):
         init_i_strategy=choice(self.i_strategies)
         init_minusi_strategy=choice(self.minusi_strategies)
         init_i_payoff=[x for x in init_i_strategy if -x in init_minusi_strategy][0]
@@ -34,25 +34,53 @@ class ZeroSumGame(object):
         counter_i_strategy=1
         counter_minusi_strategy=1
         counter=0
-        while something>epsilon:
+
+        temp_empirical_mixed_strategy_i=counter_i_strategy
+        temp_empirical_mixed_strategy_minusi=counter_minusi_strategy
+        difference_1=5
+        difference_2=5
+
+
+        while difference_1>epsilon_1 and difference_2>epsilon_2:
             counter+=1
             empirical_mixed_strategy_i=counter_i_strategy/counter
             empirical_mixed_strategy_minusi=counter_minusi_strategy/counter
-            i_payoff_1=[x for x in self.i_strategies[0] if -x in init_minusi_strategy][0]*empirical_mixed_strategy_minusi
-            i_payoff_2=[x for x in self.i_strategies[1] if -x in init_minusi_strategy][0]*empirical_mixed_strategy_minusi
+            i_payoff_1=[x for x in (self.i_strategies[0]*empirical_mixed_strategy_minusi) if -x in init_minusi_strategy*empirical_mixed_strategy_minusi][0]
+            i_payoff_2=[x for x in self.i_strategies[1]*empirical_mixed_strategy_minusi if -x in init_minusi_strategy*empirical_mixed_strategy_minusi][0]
 
-            minusi_payoff=[x for x in minusi_strategy if -x in i_strategy][0]
+            if i_payoff_1>i_payoff_2:
+                i_best_response=self.i_strategies[0]
+            else:
+                i_best_response=self.i_strategies[1]
 
-            if empirical_mixed_strategy_minusi*
-            i_strategy=empirical_mixed_strategy_minusi*
+            minusi_payoff_1=[x for x in (self.minusi_strategies[0]*empirical_mixed_strategy_i) if -x in init_i_strategy*empirical_mixed_strategy_i][0]
+            minusi_payoff_2=[x for x in self.minusi_strategies[1]*empirical_mixed_strategy_i if -x in init_i_strategy*empirical_mixed_strategy_i][0]
 
-            if minus_i_strategy==init_minusi_strategy:
-                counter_minusi_strategy+=1
-            if i_strategy==init_i_strategy:
+            if minusi_payoff_1>minusi_payoff_2:
+                minusi_best_response=self.minusi_strategies[0]
+            else:
+                minusi_best_response=self.minusi_strategies[1]
+
+            i_strategy=i_best_response
+            minus_i_strategy=minusi_best_response
+
+            if np.array_equal(i_strategy, init_i_strategy):
                 counter_i_strategy+=1
+
+            if np.array_equal(minus_i_strategy, init_minusi_strategy):
+                counter_minusi_strategy+=1
+
+            difference_1=abs(temp_empirical_mixed_strategy_i-empirical_mixed_strategy_i)
+            difference_2=abs(temp_empirical_mixed_strategy_minusi-empirical_mixed_strategy_minusi)
+            print(difference_1)
+            print(difference_2)
+            temp_empirical_mixed_strategy_i=empirical_mixed_strategy_i
+            temp_empirical_mixed_strategy_minusi=empirical_mixed_strategy_minusi
+            #print([temp_empirical_mixed_strategy_i,temp_empirical_mixed_strategy_minusi])
+
             pass
 
-
+        self.nash_equilibrium=[empirical_mixed_strategy_i, 1-empirical_mixed_strategy_i, 1-empirical_mixed_strategy_minusi, empirical_mixed_strategy_minusi]
 
         #print(self.minusi_payoffs)
         #print(self.minusi_temp)
@@ -61,7 +89,9 @@ class ZeroSumGame(object):
         return
 
 my_zerosumgame=ZeroSumGame(dummy_game)
-my_zerosumgame.fictitiousPlay(1)
+my_zerosumgame.fictitiousPlay(0.0001, 0.0001)
 #print(my_zerosumgame.i_strategy)
-print(my_zerosumgame.minusi_strategies)
-print(my_zerosumgame.i_strategies)
+#print(my_zerosumgame.minusi_strategies)
+#print(my_zerosumgame.i_strategies)
+print(my_zerosumgame.nash_equilibrium)
+
