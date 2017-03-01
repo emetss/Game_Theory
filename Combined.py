d025685d70c60aa34d1febcff86419501b2149e7
rappatoni <maxgrapp@googlemail.com>
2017-03-01 12:32:59 +0100

    cleanup and remove scaffoliding

--- Combined.py
+++ Combined.py
@@ -171,23 +171,12 @@ for i in range (1000):
     my_zerosumgame2=fp.ZeroSumGame(some_game)
     my_zerosumgame2.fictitiousPlay(0.0001, 0.0001)
     data_no_constraints.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.counter, my_zerosumgame2.rate_of_convergence])
-    # if my_zerosumgame2.counter==2:
-    #     twocounter+=1
-    # if my_zerosumgame2.counter==101:
-    #     hundredcounter+=1
-    #if 1 not in my_zerosumgame2.nash_equilibrium:
     if float("Inf") in my_zerosumgame2.rate_of_convergence:
         data_inf_rate_of_convergence.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.counter, my_zerosumgame2.rate_of_convergence])
     if list(filter(lambda x: x>=0.99, my_zerosumgame2.nash_equilibrium))!=[]:
         data_pure_nash_candidates.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.counter, my_zerosumgame2.rate_of_convergence])
     else:
         data_without_pure_nash.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.counter, my_zerosumgame2.rate_of_convergence])
-    #print(some_game)
-    #print(my_zerosumgame2.nash_equilibrium)
-    #print(my_zerosumgame2.counter)
-    #print(my_zerosumgame2.rate_of_convergence)
-
-#print(twocounter/hundredcounter)
 
 x=[]
 y=[]
@@ -232,8 +221,6 @@ for i in data_pure_nash_candidates:
     x.append(i[2])
     y.append(i[3])
     n_it.append(i[4])
-    #print(i)
-#print("")
 
 plt.plot(n_it, 'ro')
 plt.xlabel="n"
@@ -251,8 +238,6 @@ for i in data_without_pure_nash:
     x.append(i[2])
     y.append(i[3])
     n_it.append(i[4])
-    #print(i)
-#print("")
 
 plt.plot(n_it, 'ro')
 plt.xlabel="n"
@@ -263,20 +248,6 @@ plt.scatter(x, y)
 plt.title("Games without Pure Nash Candidates")
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 data_no_strictly_dominant_strategies=[]
 for i in range (1000):
     instance_of_generator = Generator()
@@ -285,13 +256,6 @@ for i in range (1000):
     my_zerosumgame2.fictitiousPlay(0.0001, 0.0001)
     data_no_strictly_dominant_strategies.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.rate_of_convergence])
 
-    #print(some_game)
-    #print(my_zerosumgame2.nash_equilibrium)
-    #print(my_zerosumgame2.counter)
-    #print(my_zerosumgame2.rate_of_convergence)
-
-#print(data_no_strictly_dominant_strategies)
-
 x_prime=[]
 y_prime=[]
 n_it=[]
@@ -299,7 +263,6 @@ for i in data_no_strictly_dominant_strategies:
     x_prime.append(i[2])
     y_prime.append(i[3])
     n_it.append(i[4])
-    #print(i)
 
 plt.plot(n_it, 'ro')
 plt.show()
@@ -307,80 +270,4 @@ plt.show()
 plt.scatter(x_prime, y_prime)
 plt.title("No Strictly Dominant Strategies")
 plt.show()
-#print("")
 
-data_no_NE_in_dominant_strategies=[]
-for i in range (1000):
-    instance_of_generator = Generator()
-    some_game = instance_of_generator.no_NE_in_dominant_strategies()
-    my_zerosumgame2=fp.ZeroSumGame(some_game)
-    my_zerosumgame2.fictitiousPlay(0.0001, 0.0001)
-    data_no_NE_in_dominant_strategies.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.rate_of_convergence])
-
-    #print(some_game)
-    #print(my_zerosumgame2.nash_equilibrium)
-    #print(my_zerosumgame2.counter)
-    #print(my_zerosumgame2.rate_of_convergence)
-
-
-x_pprime=[]
-y_pprime=[]
-for i in data_no_NE_in_dominant_strategies:
-    x_pprime.append(i[2])
-    y_pprime.append(i[3])
-    #print(i)
-
-plt.scatter(x_pprime, y_pprime)
-plt.title("No NE in Strictly Dominant Strategies")
-plt.show()
-
-
-data_NE_in_dominant_strategies=[]
-for i in range (1000):
-    instance_of_generator = Generator()
-    some_game = instance_of_generator.NE_in_dominant_strategies()
-    my_zerosumgame2=fp.ZeroSumGame(some_game)
-    my_zerosumgame2.fictitiousPlay(0.0001, 0.0001)
-    data_NE_in_dominant_strategies.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.rate_of_convergence])
-
-    #print(some_game)
-    #print(my_zerosumgame2.nash_equilibrium)
-    #print(my_zerosumgame2.counter)
-    #print(my_zerosumgame2.rate_of_convergence)
-
-x_ppprime=[]
-y_ppprime=[]
-for i in data_NE_in_dominant_strategies:
-    x_ppprime.append(i[2])
-    y_ppprime.append(i[3])
-    #print(i)
-#print("")
-
-plt.scatter(x_ppprime, y_ppprime)
-plt.title("NE in Dominant Strategies")
-plt.show()
-
-data_exactly_one_dominant_strategy=[]
-for i in range (1000):
-    instance_of_generator = Generator()
-    some_game = instance_of_generator.exactly_one_dominant_strategy()
-    my_zerosumgame2=fp.ZeroSumGame(some_game)
-    my_zerosumgame2.fictitiousPlay(0.0001, 0.0001)
-    data_exactly_one_dominant_strategy.append([list(some_game),my_zerosumgame2.nash_equilibrium,my_zerosumgame2.counter_row,my_zerosumgame2.counter_col,my_zerosumgame2.rate_of_convergence])
-
-    #print(some_game)
-    #print(my_zerosumgame2.nash_equilibrium)
-    #print(my_zerosumgame2.counter)
-    #print(my_zerosumgame2.rate_of_convergence)
-
-x_pppprime=[]
-y_pppprime=[]
-for i in data_exactly_one_dominant_strategy:
-    x_pppprime.append(i[2])
-    y_pppprime.append(i[3])
-    #print(i)
-#print("")
-
-plt.scatter(x_ppprime, y_ppprime)
-plt.title("Exactly one Dominant Strategy")
-plt.show()
